d865ac771baa4932788ebf924b91d24ebfedd50c
unknown <ayam28040@gmail.com>
2024-06-21 23:47:45 +0300

    handle forget password

--- components/LogIn.vue
+++ components/LogIn.vue
@@ -5,6 +5,12 @@
       <InputText
         v-model="email"
         type="email"
+        @input="
+          () => {
+            msg = false;
+            errMsg = false;
+          }
+        "
         placeholder="Your Email"
         required
         class="rounded-none"
@@ -15,13 +21,40 @@
       <Password
         v-model="pass"
         required
+        @input="
+          () => {
+            msg = false;
+            errMsg = false;
+          }
+        "
         :feedback="false"
         placeholder="Password"
         class="rounded-none w-full"
         toggleMask
       />
+      <p
+        class="underline cursor-pointer"
+        @click="
+          () => {
+            sendPasswordResetEmail(auth, email)
+              .then(() => {
+                msg = false;
+                errMsg = false;
+                msg = true;
+              })
+              .catch((error) => {
+                console.log(error);
+              });
+          }
+        "
+      >
+        Forget password
+      </p>
     </div>
     <p v-if="errMsg" class="text-red-700 text-sm">{{ errMsg }}</p>
+    <p v-if="msg" class="text-secondary-color text-sm">
+      The email has been sent to {{ email }} to reset your password.
+    </p>
 
     <Button
       label="LOGIN"
@@ -31,13 +64,20 @@
   </form>
 </template>
 <script setup>
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  sendPasswordResetEmail,
+} from "firebase/auth";
 const email = ref();
 const errMsg = ref();
 const pass = ref();
 const emits = defineEmits(["handleLogin"]);
-
+const msg = ref(false);
+const auth = getAuth();
 function submitFun(e) {
+  msg = false;
+  errMsg = false;
   e.preventDefault();
   signInWithEmailAndPassword(getAuth(), email.value, pass.value)
     .then((d) => {
@@ -45,7 +85,8 @@ function submitFun(e) {
       useCookie("user").value = d.user.accessToken;
       emits("handleLogin");
     })
-    .catch(() => {
+    .catch((err) => {
+      console.group(err.message);
       errMsg.value = "Email or password was incorrect";
     });
 }
